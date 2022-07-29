a3afebaf934743f24dd499971b58bae2a44066db
KitaharaMugiro <you@example.com>
2022-07-29 16:29:50 +0900

    一旦コミット

---
+++ contracts/ContributionPoll.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "hardhat/console.sol";
+
+contract ContributionPoll is AccessControl {
+    int256 pollId = 0;
+
+    /**
+     * @notice Settle the current poll, and start new poll
+     */
+    function settleCurrentAndCreateNewAuction() external {
+        _settleContributionPoll();
+        _createContributionPoll();
+    }
+
+    /**
+     * @notice Settle the current poll
+     */
+    function _settleContributionPoll() internal {
+        // TODO : 貢献度投票の集計コントラクトを呼び出す
+    }
+
+    /**
+     * @notice start new poll
+     */
+    function _createContributionPoll() internal {
+        pollId++;
+    }
+
+    /**
+     * @notice candidate to the current poll
+     */
+    function candidateToContributionPoll() external {
+        // TODO
+    }
+
+    /**
+     * @notice vote to the current poll
+     */
+    function vote(address candidate) external {
+        // TODO
+    }
+}
---
+++ test/ContributionPoll.ts
@@ -0,0 +1,26 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("ContributionPoll", function () {
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshopt in every test.
+    async function deploy() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
+
+        const ContributionPoll = await ethers.getContractFactory("ContributionPoll");
+        const poll = await ContributionPoll.deploy();
+
+        return { poll, owner, otherAccount };
+    }
+
+    describe("Deployment", function () {
+        it("poll Idの初期値は0", async function () {
+            const { poll } = await loadFixture(deploy);
+            expect(await poll.pollId).to.equal(0);
+        });
+    });
+});
