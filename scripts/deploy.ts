d0414a8caa849331b221837feafdd56f8537efd9
KitaharaMugiro <you@example.com>
2022-09-12 00:01:48 +0900

    deploy script作成

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -31,10 +31,26 @@ async function main() {
   await tokenSupplySystem.deployed();
   console.log("TokenSupplySystem deployed to", tokenSupplySystem.address);
 
+  // Deploy DaoNft and CrowdSale
+  const DaoNft = await ethers.getContractFactory("DAONFT");
+  const NFT_NAME = "EnglisterDAOMembership"
+  const NFT_SYMBOL = "EDM"
+  const BASE_URI = "https://raw.githubusercontent.com/KitaharaMugiro/englisterdao/main/contracts/metadata/daonft/"
+  const nft = await DaoNft.deploy(NFT_NAME, NFT_SYMBOL, BASE_URI);
+  await nft.deployed();
+  console.log("DAONFT deployed to", nft.address);
+
+  const DaoNftCrowdSale = await ethers.getContractFactory("DAONFTCrowdSale");
+  const nftCrowdSale = await DaoNftCrowdSale.deploy();
+  await nftCrowdSale.deployed();
+  console.log("DAONFTCrowdSale deployed to", nftCrowdSale.address);
+
+
   // 権限設定
   await token.setupBurnerRole(treasury.address);
   await token.setupMinterRole(poll.address);
   await token.setupMinterRole(tokenSupplySystem.address);
+  await token.setupBurnerRole(nftCrowdSale.address);
 
   await treasury.setDAOTokenAddress(token.address);
 
@@ -44,7 +60,12 @@ async function main() {
   await tokenSupplySystem.setDAOTokenAddress(token.address);
   await tokenSupplySystem.setDAOTreasuryAddress(treasury.address);
 
+  await nftCrowdSale.setDAOTokenAddress(token.address);
+  await nftCrowdSale.setDAONftAddress(nft.address);
 
+  await nft.setupMinterRole(nftCrowdSale.address);
+
+  await nftCrowdSale.setPrice(ethers.utils.parseEther("500"));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
