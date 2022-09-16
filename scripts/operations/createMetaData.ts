fa9ab98d074ebe6fe731bd52acccd0d578823306
KitaharaMugiro <you@example.com>
2022-09-16 12:35:59 +0900

    fix metadata

--- scripts/operations/createMetaData.ts
+++ scripts/operations/createMetaData.ts
@@ -5,7 +5,7 @@ const fs = require('fs');
 for (let index = 0; index < 100; index++) {
     let metadata = {
         "tokenId": String(index),
-        "name": "EnglisterDAOメンバーシップ証",
+        "name": `EnglisterDAO Membership #${index}`,
         "description": "EnglisterDAOのメンバーである証。DAO投票のために使用できます。",
         "image": `https://yunomy-image-folder.s3.ap-northeast-1.amazonaws.com/englister/dao_membership/${index}.png`
     };
