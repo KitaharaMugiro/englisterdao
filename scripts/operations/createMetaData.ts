0dc6accdf9c1a23a66af607bb0326223a5264ae7
KitaharaMugiro <you@example.com>
2022-10-07 13:20:02 +0900

    metadataa更新

--- scripts/operations/createMetaData.ts
+++ scripts/operations/createMetaData.ts
@@ -3,11 +3,13 @@
 const fs = require('fs');
 
 for (let index = 0; index < 100; index++) {
+    let num = ('000' + index).slice(-4);
+    let imageFileName = `DAOmember_${num}.png`;
     let metadata = {
         "tokenId": String(index),
         "name": `EnglisterDAO Membership #${index}`,
         "description": "EnglisterDAOのメンバーである証。DAO投票のために使用できます。",
-        "image": `https://yunomy-image-folder.s3.ap-northeast-1.amazonaws.com/englister/dao_membership/${index}.png`
+        "image": `https://yunomy-image-folder.s3.ap-northeast-1.amazonaws.com/englister/dao_membership/${imageFileName}`
     };
     let data = JSON.stringify(metadata);
     let path = `./contracts/metadata/daonft/${index}`;
